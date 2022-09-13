d17b8a476a4da02e189ed67d108e65391b0d367a
shreya_aiyer <f20210014@goa.bits-pilani.ac.in>
2022-09-13 15:04:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/about_me/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.about_me
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.about_me.databinding.ActivityMainBinding
+import com.example.android.about_me.MyName
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    private val myName: MyName = MyName("Shreya Aiyer")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //findViewById<Button>(R.id.done_button).setOnClickListener{
+            //addNickname(it)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        binding.apply {
+            nicknameTextView.text = editText.text
+            invalidateAll()
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+        }
+        myName?.nickname = nickname_edit.text.toString()
+        // Invalidate all binding expressions and request a new rebind to refresh UI
+        invalidateAll()
+        //Hiding keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
